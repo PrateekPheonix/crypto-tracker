e95d42437e09261beb40d50fc4a3cb3af8292d04
Prateek Niket <prateekniket25@gmail.com>
2021-10-08 20:20:16 +0530

    Update Coin.js

--- src/Coin.js
+++ src/Coin.js
@@ -15,7 +15,7 @@ const Coin = ({image, name, symbol, price, volume, priceChange, marketcap}) => {
                     <p className="coin-volume">₹ {volume.toLocaleString()}</p>
                     {priceChange < 0 
                         ?(
-                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                        <p className="coin-percent red">+{priceChange.toFixed(2)}%</p>
                         )
                         :(
                             <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
